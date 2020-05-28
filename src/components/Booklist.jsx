20a2609eaf5f197d8e0368f50449442e5c91415b
すとぅー <59303958+bellneko@users.noreply.github.com>
2020-05-29 04:47:17 +0900

    課題提出用

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,26 @@
+// Booklist.jsx
+import React, { useState, useEffect } from 'react';
+
+const Booklist = (props) => {
+    const [bookData, setBookData] = useState(null);
+    useEffect(() => {
+      const result = props.getData?.(props.language).then((response) => setBookData(response));}, [props]);
+    return (
+      <div>
+        <ul>
+          {bookData === null ? (
+            <p>now loading...</p>
+          ) : (
+            bookData.data.items.map((x, index) => (
+              <li key={index}>
+                {x.volumeInfo.title} / {x.volumeInfo.authors} / {x.volumeInfo.publisher}
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    );
+  };
+  export default Booklist;
+
+
